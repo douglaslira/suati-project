a01d32292a62ed8fdafa83514acdd0ba926c72ba
Douglas Lira <douglas.lira.web@gmail.com>
2017-05-16 16:26:03 -0300

    Create orderby

---
+++ app/components/orderbyobject/orderbyobject.js
@@ -0,0 +1,29 @@
+(function(){
+
+    'use strict';
+
+    angular.module('suatiApp.components').filter('orderObjectBy', orderObjectBy);
+
+    orderObjectBy.$inject = [];
+
+    function orderObjectBy(){
+        return function(input, attribute) {
+
+            if (!angular.isObject(input)) {
+                return input;
+            }
+
+            var array = [];
+            for (var objectKey in input) {
+                array.push(input[objectKey]);
+            }
+
+            array.sort(function (a, b) {
+                a = parseInt(a[attribute]);
+                b = parseInt(b[attribute]);
+                return a - b;
+            });
+            return array;
+        }
+    }
+});
--- app/modules/home/controllers/homeController.js
+++ app/modules/home/controllers/homeController.js
@@ -7,14 +7,36 @@
     HomeController.$inject = ['$scope', 'CoreService'];
 
     function HomeController(scope, CoreService){
+
         scope.hello = 'Hello World!!';
+        scope.listOfContracts = [];
+        scope.sort = {
+            column: 'Código',
+            descending: false
+        };
+
+
         scope.init = function(){
             CoreService.getData().get().$promise.then(function(res){
-                console.log(res.contracts);
+                scope.listOfContracts = res.contracts;
             }, function(error){
                 console.log(error);
             })
-        }
+        };
+
+        scope.selectedCls = function(column) {
+            return column == scope.sort.column && 'sort-' + scope.sort.descending;
+        };
+
+        scope.changeSorting = function(column) {
+            var sort = scope.sort;
+            if (sort.column == column) {
+                sort.descending = !sort.descending;
+            } else {
+                sort.column = column;
+                sort.descending = false;
+            }
+        };
     }
 
 })();
